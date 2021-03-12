1dc37878fe0dbd30ad9079b5b16a78f097d4c1de
kangsudal-pc1 <kangsudal@gmail.com>
2021-03-12 16:07:46 +0900

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,13 +19,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _resultText;//출력화면-결과값
+  String _formulaText;//출력화면-계산식
+
+  void _changeFormulaText(){ //
+    setState(() {
+
+    });
+  }
+  void _changeResultText(){ //
+    setState(() {
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double screenWidth = screenSize.width;
+    double screenHeight = screenSize.height;
     return Scaffold(
-      body: Center(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          OutputContainer(screenHeight, screenWidth),//출력화면
+          DeleteButton(),//삭제버튼
+          Divider(),
+          Expanded(child: InputContainer()),//입력키버튼
+        ],
       ),
     );
   }
+
+  Widget OutputContainer(double screenHeight, screenWidth){
+    return Container(
+      height: screenHeight*0.3,
+      width: screenWidth,
+    );
+  }
+  Widget DeleteButton(){
+    return IconButton(icon: Icon(Icons.backspace), onPressed: (){});
+  }
+  Widget InputContainer(){
+    return GridView.count(crossAxisCount: 4);
+  }
+
 }
