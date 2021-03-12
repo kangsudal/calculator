0c1df6218f264dcf79529b53d24cc199c22701e1
kangsudal-pc1 <kangsudal@gmail.com>
2021-03-12 17:24:57 +0900

    계산식 출력화면, 입력버튼 구성중

--- lib/main.dart
+++ lib/main.dart
@@ -20,24 +20,22 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _resultText;//출력화면-결과값
-  String _formulaText;//출력화면-계산식
+  String _resultText = '계산결과22'; //출력화면-결과값
+  String _formulaText; //출력화면-계산식
 
-  void _changeFormulaText(){ //
-    setState(() {
-
-    });
+  void _changeFormulaText() {
+    //
+    setState(() {});
   }
-  void _changeResultText(){ //
-    setState(() {
 
-    });
+  void _changeResultText() {
+    //
+    setState(() {});
   }
 
   @override
@@ -49,26 +47,47 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          OutputContainer(screenHeight, screenWidth),//출력화면
-          DeleteButton(),//삭제버튼
+          OutputContainer(screenHeight, screenWidth), //출력화면
+          DeleteButton(), //삭제버튼
           Divider(),
-          Expanded(child: InputContainer()),//입력키버튼
+          Expanded(child: InputContainer()), //입력키버튼
         ],
       ),
     );
   }
 
-  Widget OutputContainer(double screenHeight, screenWidth){
+  Widget OutputContainer(double screenHeight, screenWidth) {
     return Container(
-      height: screenHeight*0.3,
+      height: screenHeight * 0.3,
       width: screenWidth,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          //입력계산식 보여주는 화면
+          TextField(
+            onChanged: (String val) => _formulaText = val,
+          ),
+          //입력계산결과 보여주는 화면
+          Text(_resultText),
+        ],
+      ),
     );
   }
-  Widget DeleteButton(){
-    return IconButton(icon: Icon(Icons.backspace), onPressed: (){});
-  }
-  Widget InputContainer(){
-    return GridView.count(crossAxisCount: 4);
+
+  Widget DeleteButton() {
+    return IconButton(icon: Icon(Icons.backspace), onPressed: () {});
   }
 
+  Widget InputContainer() {
+    List val_list = List.generate(10, (index) => index);
+    List keyboard_list = ([
+      1,2,3,4,5,6,7,8,9,0,
+    ].map((val) =>
+        TextButton(onPressed: _changeFormulaText, child: Text("$val")))).toList();
+    return GridView.count(
+      physics: new NeverScrollableScrollPhysics(),
+      crossAxisCount: 3,
+      children: keyboard_list,
+    );
+  }
 }
